7422c5a3f30030b9fdf845bc08ccd39fe55e7ce2
Ali <modaresimr@gmail.com>
2022-08-01 15:02:38 +0200

    wordpress_saml

--- onelogin-saml-sso/onelogin_saml.php
+++ onelogin-saml-sso/onelogin_saml.php
@@ -1,44 +1,45 @@
 <?php
 /*
-Plugin Name: OneLogin SAML SSO
-Plugin URI: https://github.com/onelogin/wordpress-saml
+Plugin Name: Wordpress SAML SSO
+Plugin URI: https://github.com/modaresimr/wordpress-saml
 Description: Give users secure one-click access to WordPress from OneLogin. This SAML integration eliminates passwords and allows you to authenticate users against your existing Active Directory or LDAP server as well increase security using YubiKeys or VeriSign VIP Access, browser PKI certificates and OneLogin's flexible security policies. OneLogin is pre-integrated with thousands of apps and handles all of your SSO needs in the cloud and behind the firewall.
 Author: OneLogin, Inc.
 Version: 3.4.0
-Author URI: http://www.onelogin.com
+Author URI: https://github.com/modaresimr/wordpress-saml
+GitHub Plugin URI: https://github.com/modaresimr/wordpress-saml
 */
 
 // Make sure we don't expose any info if called directly
-if ( !function_exists( 'add_action' ) ) {
+if (!function_exists('add_action')) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
 }
 
 // Allow cookie name overriding by defining following constants prior this point. Eg.: in wp-config.php.
-if (false === defined('SAML_LOGIN_COOKIE' )) {
-	define( 'SAML_LOGIN_COOKIE', 'saml_login');
+if (false === defined('SAML_LOGIN_COOKIE')) {
+	define('SAML_LOGIN_COOKIE', 'saml_login');
 }
 if (false === defined('SAML_NAMEID_COOKIE')) {
-	define( 'SAML_NAMEID_COOKIE', 'saml_nameid');
+	define('SAML_NAMEID_COOKIE', 'saml_nameid');
 }
 if (false === defined('SAML_SESSIONINDEX_COOKIE')) {
-	define( 'SAML_SESSIONINDEX_COOKIE', 'saml_sessionindex');
+	define('SAML_SESSIONINDEX_COOKIE', 'saml_sessionindex');
 }
 if (false === defined('SAML_NAMEID_FORMAT_COOKIE')) {
-	define( 'SAML_NAMEID_FORMAT_COOKIE', 'saml_nameid_format');
+	define('SAML_NAMEID_FORMAT_COOKIE', 'saml_nameid_format');
 }
 if (false === defined('SAML_NAMEID_NAME_QUALIFIER_COOKIE')) {
-	define( 'SAML_NAMEID_NAME_QUALIFIER_COOKIE', 'saml_nameid_name_qualifier');
+	define('SAML_NAMEID_NAME_QUALIFIER_COOKIE', 'saml_nameid_name_qualifier');
 }
 if (false === defined('SAML_NAMEID_SP_NAME_QUALIFIER_COOKIE')) {
-	define( 'SAML_NAMEID_SP_NAME_QUALIFIER_COOKIE', 'saml_nameid_name_sp_qualifier');
+	define('SAML_NAMEID_SP_NAME_QUALIFIER_COOKIE', 'saml_nameid_name_sp_qualifier');
 }
 
-require_once plugin_dir_path(__FILE__)."php/functions.php";
-require_once plugin_dir_path(__FILE__)."php/configuration.php";
+require_once plugin_dir_path(__FILE__) . "php/functions.php";
+require_once plugin_dir_path(__FILE__) . "php/configuration.php";
 
 // Localization
-add_action( 'init', 'saml_load_translations');
+add_action('init', 'saml_load_translations');
 
 // add menu option for configuration
 add_action('admin_menu', 'onelogin_saml_configuration');
@@ -61,11 +62,14 @@ if (!is_saml_enabled()) {
 
 $prevent_reset_password = get_option('onelogin_saml_customize_action_prevent_reset_password', false);
 if ($prevent_reset_password) {
-	add_filter ('allow_password_reset', 'disable_password_reset' );
-	function disable_password_reset() { return false; }
+	add_filter('allow_password_reset', 'disable_password_reset');
+	function disable_password_reset()
+	{
+		return false;
+	}
 } else {
 	add_action('lost_password', 'saml_lostpassword', 1);
-	add_action('retrieve_password', 'saml_lostpassword' , 1);
+	add_action('retrieve_password', 'saml_lostpassword', 1);
 	add_action('password_reset', 'saml_lostpassword', 1);
 }
 
@@ -102,7 +106,7 @@ if (isset($_GET['saml_sso'])) {
 	} else if ($local_wp_actions) {
 		$prevent_local_login = get_option('onelogin_saml_customize_action_prevent_local_login', false);
 
-		if (($want_to_local_login && $prevent_local_login) || ($want_to_reset && $prevent_reset_password)) {		
+		if (($want_to_local_login && $prevent_local_login) || ($want_to_reset && $prevent_reset_password)) {
 			$execute_sso = True;
 		}
 	}
@@ -119,12 +123,13 @@ if (isset($_GET['saml_sso'])) {
 add_action('register_form', 'saml_user_register', 1);
 
 
-function onelogin_enqueue_script() {
-	wp_enqueue_script( 'onelogin-hide-login-form', plugins_url( 'assets/js/hide-login-form.js', __FILE__ ), array('jquery'), null, true );
+function onelogin_enqueue_script()
+{
+	wp_enqueue_script('onelogin-hide-login-form', plugins_url('assets/js/hide-login-form.js', __FILE__), array('jquery'), null, true);
 }
 
 if ((strpos($_SERVER['SCRIPT_NAME'], 'wp-login.php') !== FALSE) && $action == 'login' && !isset($_GET['normal'])) {
 	if (!get_option('onelogin_saml_keep_local_login', false)) {
-		add_action( 'login_enqueue_scripts', 'onelogin_enqueue_script', 10 );
+		add_action('login_enqueue_scripts', 'onelogin_enqueue_script', 10);
 	}
 }
